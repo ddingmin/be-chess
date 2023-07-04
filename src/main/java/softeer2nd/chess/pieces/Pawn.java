bb4a2f7ee09f3f4c74b0018e0d50bf700a9b6767
ddingmin <ddingminyt@gmail.com>
2023-07-04 17:28:13 +0900

    test: "좌표 테스트 구현"

--- src/main/java/softeer2nd/chess/pieces/Pawn.java
+++ src/main/java/softeer2nd/chess/pieces/Pawn.java
@@ -6,7 +6,6 @@ public class Pawn {
     public static final char WHITE_REPRESENTATION = 'p';
     public static final char BLACK_REPRESENTATION = 'P';
 
-
     private final String color;
     private final char representation;
 
