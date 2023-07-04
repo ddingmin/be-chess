3595575b9b4e51b22da2bf18aa275b1f00132310
ddingmin <ddingminyt@gmail.com>
2023-07-05 01:30:26 +0900

    feat: "게임 시작 기능 구현"

---
+++ src/main/java/softeer2nd/chess/GameController.java
@@ -0,0 +1,64 @@
+package softeer2nd.chess;
+
+import java.util.Scanner;
+
+public class GameController {
+    public static final String START_COMMAND_MESSAGE = "다음 커맨드를 사용해 주세요. (시작: start, 종료 end)";
+    public static final String GAME_COMMAND_ERROR = "잘못된 입력입니다. 다시 입력해주세요.";
+    public static final String GAME_START_MESSAGE = "체스 게임을 시작합니다.";
+    public static final String GAME_END_MESSAGE = "체스 게임을 종료합니다.";
+    public static final String GAME_EXIT_MESSAGE = "체스 게임 프로그램을 종료합니다.";
+    public static final String SHOW_BOARD_MESSAGE = "== 진행 상황 ==";
+    public static final String START = "START";
+    public static final String END = "END";
+    private Board board;
+    private String command;
+    private boolean isGaming;
+
+    Scanner sc = new Scanner(System.in);
+
+    public void main() {
+        isGaming = true;
+
+        while (isGaming) {
+            System.out.println(START_COMMAND_MESSAGE);
+            command = sc.nextLine();
+            while (!isValidCommand(command)) {
+                System.out.println(GAME_COMMAND_ERROR);
+                command = sc.nextLine();
+            }
+            if (command.toUpperCase().equals(START)) {
+                startGame();
+                System.out.println(GAME_END_MESSAGE);
+            }
+            if (command.toUpperCase().equals(END)) {
+                System.out.println(GAME_EXIT_MESSAGE);
+                break;
+            }
+        }
+    }
+
+    private void startGame() {
+        System.out.println(GAME_START_MESSAGE);
+        board = new Board();
+        board.initialize();
+        System.out.println(SHOW_BOARD_MESSAGE);
+        System.out.println(board.getBoardResult());
+        //TODO: 게임 로직을 추가로 구현해야 한다.
+    }
+
+    private boolean isValidCommand(String command) {
+        if (command == null) {
+            return false;
+        }
+        for (int i = 0; i < command.length(); i++) {
+            if (!Character.isAlphabetic(command.charAt(i))) {
+                return false;
+            }
+        }
+        if (command.toUpperCase().equals(START) || command.toUpperCase().equals(END)) {
+            return true;
+        }
+        return false;
+    }
+}
