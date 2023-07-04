e3f53305a0610907946dc73bf59819af6133043a
ddingmin <ddingminyt@gmail.com>
2023-07-04 17:47:56 +0900

    test: "체스판 생성, 출력 테스트 구현"

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -20,10 +20,12 @@ class BoardTest {
     @DisplayName("보드 클래스를 테스트 한다. size(), findPawn() 메서드를 테스트한다.")
     void create() throws Exception {
         Pawn white = new Pawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
-        verifyAddPawn(white, 1, 0);
+        Point pointWhite = new Point(1, 8);
+        verifyAddPawn(white, 1, 0, pointWhite);
 
         Pawn black = new Pawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
-        verifyAddPawn(black, 2, 1);
+        Point pointBlack = new Point(2, 7);
+        verifyAddPawn(black, 2, 1, pointBlack);
     }
 
     @Test
@@ -32,12 +34,36 @@ class BoardTest {
 //        board.add(new Integer(7));
     }
 
-    void addPawn(Board board, Pawn pawn) {
-        board.add(pawn);
+    @Test
+    @DisplayName("보드의 생성 테스트")
+    void initialize() throws Exception {
+        board.initialize();
+        assertEquals("pppppppp", board.getWhitePawnsResult());
+        assertEquals("PPPPPPPP", board.getBlackPawnsResult());
     }
 
-    void verifyAddPawn(Pawn pawn, int size, int index) {
-        addPawn(board, pawn);
+    @Test
+    @DisplayName("보드의 출력 테스트")
+    void print() {
+        String correctPrint = "........\n" +
+                "PPPPPPPP\n" +
+                "........\n" +
+                "........\n" +
+                "........\n" +
+                "........\n" +
+                "pppppppp\n" +
+                "........\n";
+        board.initialize();
+        assertEquals(board.toString(), correctPrint);
+        System.out.println(board.toString());
+    }
+
+    void addPawn(Board board, Pawn pawn, Point point) {
+        board.add(pawn, point);
+    }
+
+    void verifyAddPawn(Pawn pawn, int size, int index, Point point) {
+        addPawn(board, pawn, point);
         assertEquals(size, board.size());
         assertEquals(pawn, board.findPawn(index));
     }
