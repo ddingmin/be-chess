762dad035eab5e3bfdb6d93bc417c2a472d4f0f3
ddingmin <ddingminyt@gmail.com>
2023-07-06 07:28:33 +0900

    test: "체스판 초기화 테스트 작성"

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -6,63 +6,42 @@ import org.junit.jupiter.api.Test;
 import softeer2nd.chess.pieces.Piece;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static softeer2nd.chess.utils.StringUtils.appendNewLine;
 
 @DisplayName("체스판 테스트")
-class BoardTest {
-    Board board;
+public class BoardTest {
+    private Board board;
 
     @BeforeEach
-    void setUp() {
+    public void setup() {
         board = new Board();
     }
 
     @Test
-    @DisplayName("보드 클래스를 테스트 한다. size(), findPawn() 메서드를 테스트한다.")
-    void create() throws Exception {
-        Piece whitePawn = Piece.createWhitePawn();
-        verifyAddPawn(whitePawn, 1, 0, 1, 8);
+    @DisplayName("기물들은 주어진 좌표에 생성된다.")
+    void add() {
+        String point = "13";
 
-        Piece blackPawn = Piece.createBlackPawn();
-        verifyAddPawn(blackPawn, 2, 1, 2, 7);
+        board.add(point, Piece.createBlackKing());
+
+        assertEquals(1, board.pieceCount());
     }
 
     @Test
-    @DisplayName("보드에 폰 이외의 객체는 추가할 수 없다.")
-    void create_다른객체() {
-//        board.add(new Integer(7));
-    }
-
-    @Test
-    @DisplayName("보드의 생성 테스트")
-    void initialize() throws Exception {
+    @DisplayName("체스판의 초기 기물 수는 32개, 각 위치의 올바르게 초기화 되어야 한다.")
+    public void create() throws Exception {
         board.initialize();
-        assertEquals("pppppppp", board.getWhitePawnsResult());
-        assertEquals("PPPPPPPP", board.getBlackPawnsResult());
-    }
 
-    @Test
-    @DisplayName("보드의 출력 테스트")
-    void print() {
-        String correctPrint = "........\n" +
-                "PPPPPPPP\n" +
-                "........\n" +
-                "........\n" +
-                "........\n" +
-                "........\n" +
-                "pppppppp\n" +
-                "........";
-        board.initialize();
-        assertEquals(board.getBoardResult(), correctPrint);
-        System.out.println(board.getBoardResult());
-    }
+        assertEquals(32, board.pieceCount());
 
-    void addPawn(Board board, Piece piece, int x, int y) {
-        board.add(piece, x, y);
-    }
+        String blankRank = appendNewLine("........");
 
-    void verifyAddPawn(Piece piece, int size, int index, int x, int y) {
-        addPawn(board, piece, x, y);
-        assertEquals(size, board.size());
-        assertEquals(piece, board.findPawn(index));
+        assertEquals(
+                appendNewLine("RNBQKBNR") +
+                        appendNewLine("PPPPPPPP") +
+                        blankRank + blankRank + blankRank + blankRank +
+                        appendNewLine("pppppppp") +
+                        appendNewLine("rnbqkbnr"),
+                board.showBoard());
     }
 }
