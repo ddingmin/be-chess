d6226d0c3980c3d4b70aeaa67642b66723adb0a8
ddingmin <ddingminyt@gmail.com>
2023-07-06 13:14:18 +0900

    test: "기물 enum 구현 테스트 작성"

--- src/test/java/softeer2nd/chess/pieces/PieceTest.java
+++ src/test/java/softeer2nd/chess/pieces/PieceTest.java
@@ -5,20 +5,26 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static softeer2nd.chess.utils.PieceUtils.*;
 
 @DisplayName("기물 테스트")
 class PieceTest {
     @Test
     @DisplayName("입력한 색의 기물이 생성되어야 한다, 기물과 색에 따라 올바른 출력을 해야 한다.")
     void create_piece() {
-        verifyPiece(Piece.createWhitePawn(), WHITE, WHITE_PAWN_REPRESENTATION);
-        verifyPiece(Piece.createBlackPawn(), BLACK, BLACK_PAWN_REPRESENTATION);
-        verifyPiece(Piece.createWhiteKing(), WHITE, WHITE_KING_REPRESENTATION);
-        verifyPiece(Piece.createWhiteQueen(), WHITE, WHITE_QUEEN_REPRESENTATION);
-        verifyPiece(Piece.createWhiteBishop(), WHITE, WHITE_BISHOP_REPRESENTATION);
-        verifyPiece(Piece.createWhiteRook(), WHITE, WHITE_ROOK_REPRESENTATION);
-        verifyPiece(Piece.createWhiteKnight(), WHITE, WHITE_KNIGHT_REPRESENTATION);
+        verifyPiece(Piece.createBlackPawn(), Piece.Color.BLACK, Piece.Type.PAWN.getBlackRepresentation());
+        verifyPiece(Piece.createWhitePawn(), Piece.Color.WHITE, Piece.Type.PAWN.getWhiteRepresentation());
+        verifyPiece(Piece.createWhiteKing(), Piece.Color.WHITE, Piece.Type.KING.getWhiteRepresentation());
+        verifyPiece(Piece.createWhiteQueen(), Piece.Color.WHITE, Piece.Type.QUEEN.getWhiteRepresentation());
+        verifyPiece(Piece.createWhiteBishop(), Piece.Color.WHITE, Piece.Type.BISHOP.getWhiteRepresentation());
+        verifyPiece(Piece.createWhiteRook(), Piece.Color.WHITE, Piece.Type.ROOK.getWhiteRepresentation());
+        verifyPiece(Piece.createWhiteKnight(), Piece.Color.WHITE, Piece.Type.KNIGHT.getWhiteRepresentation());
+    }
+
+    @Test
+    @DisplayName("기물의 색에 따라 올바른 representation 을 반환해야 한다.")
+    public void getRepresentationPerPiece() throws Exception {
+        assertEquals('p', Piece.Type.PAWN.getWhiteRepresentation());
+        assertEquals('P', Piece.Type.PAWN.getBlackRepresentation());
     }
 
     @Test
@@ -31,7 +37,7 @@ class PieceTest {
         assertTrue(blackPawn.isBlack());
     }
 
-    private void verifyPiece(final Piece piece, final String color, final char representation) {
+    private void verifyPiece(final Piece piece, final Piece.Color color, final char representation) {
         assertEquals(color, piece.getColor());
         assertEquals(representation, piece.getRepresentation());
     }
