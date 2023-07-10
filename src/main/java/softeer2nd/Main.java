bacff762c875a075b66678d71e64b471fea662e8
ddingmin <ddingminyt@gmail.com>
2023-07-10 13:10:35 +0900

    refactor: "main에 시작 로직 구현하도록 리팩토링"
    - 실행 관련 로직들은 기존의 ChessGame에서 main으로 이동
    - 입력을 받고 출력하는 Command 클래스 삭제

--- src/main/java/softeer2nd/Main.java
+++ src/main/java/softeer2nd/Main.java
@@ -2,11 +2,73 @@ package softeer2nd;
 
 import softeer2nd.chess.ChessGame;
 import softeer2nd.chess.board.Board;
+import softeer2nd.chess.board.Position;
+import softeer2nd.chess.view.View;
+
+import java.util.Scanner;
 
 public class Main {
+    public static final String START = "start";
+    public static final String END = "end";
+    public static final String MOVE = "move";
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Board board = new Board();
-        ChessGame gameController = new ChessGame(board);
-        gameController.main();
+        ChessGame chessGame = new ChessGame(board);
+        View view = new View(board);
+        String command;
+
+        boolean isRunning = true;
+        boolean isGaming;
+
+        while (isRunning) {
+            view.printProgramCommand();
+            command = scanner.nextLine();
+
+            if (!isCorrectProgramCommand(command)) {
+                view.printError();
+                continue;
+            }
+
+            if (command.equals(END)) {
+                view.printProgramEnd();
+                isRunning = false;
+            }
+            if (command.equals(START)) {
+                isGaming = true;
+                chessGame.initialize();
+                view.printBoard();
+
+                while (isGaming) {
+                    view.printGameCommand();
+                    command = scanner.nextLine();
+
+                    if (!isCorrectGameCommand(command)) {
+                        view.printError();
+                        continue;
+                    }
+
+                    if (command.equals(END)) {
+                        view.printGameEnd();
+                        isGaming = false;
+                    }
+                    if (command.startsWith(MOVE)) {
+                        view.printGameCommand();
+                        String[] moves = command.split(" ");
+                        chessGame.move(new Position(moves[1]), new Position(moves[2]));
+                        view.printBoard();
+                    }
+                }
+            }
+        }
+    }
+
+    private static boolean isCorrectGameCommand(String command) {
+        return command.equals(END) || command.startsWith(MOVE);
+    }
+
+    private static boolean isCorrectProgramCommand(String command) {
+        return command.equals(START) || command.equals(END);
     }
 }
--- src/main/java/softeer2nd/chess/ChessGame.java
+++ src/main/java/softeer2nd/chess/ChessGame.java
@@ -4,70 +4,19 @@ import softeer2nd.chess.board.Board;
 import softeer2nd.chess.board.Position;
 import softeer2nd.chess.pieces.Piece;
 
-import static softeer2nd.chess.Command.*;
-
 public class ChessGame {
     private Board board;
-    private boolean isRunning;
 
     public ChessGame(Board board) {
         this.board = board;
     }
 
-    public void main() {
-        isRunning = true;
-        String command;
-        while (isRunning) {
-            command = readRun();
-            if (command.equals(START)) {
-                startGame();
-                endGame();
-            }
-            if (command.equals(END)) {
-                exitGame();
-            }
-        }
-    }
-
-    private void startGame() {
-        boolean isGaming = true;
-        showStartGameMessage();
+    public void initialize() {
         board.initialize();
-        showGameBoard(board);
-
-        String command;
-        while (isGaming) {
-            command = readGame();
-            if (command.equals(END)) {
-                isGaming = false;
-            }
-            if (command.startsWith(MOVE)) {
-                String[] inputs = command.split(" ");
-                try {
-                    move(new Position(inputs[1]), new Position(inputs[2]));
-                    showGameBoard(board);
-                }
-                catch (IllegalArgumentException exception) {
-                    System.out.println(POSITION_INPUT_ERROR);
-                }
-            }
-        }
     }
 
-    private void endGame() {
-        showEndGameMessage();
-    }
-
-    private void exitGame() {
-        showExitGameMessage();
-        isRunning = false;
-    }
     public void move(Position sourcePosition, Position targetPosition) {
         board.put(board.findPiece(sourcePosition), targetPosition);
         board.put(Piece.createBlank(), sourcePosition);
-    }
-
-    public void initialize() {
-        board.initialize();
     }
 }
